b64fb50415149610bf3f6c63e98517fa581ae89d
Riadh HAJ AMOR <rhadjamor@gmail.com>
2022-05-21 04:32:45 +0200

    Update runner.js

--- runner.js
+++ runner.js
@@ -2,11 +2,11 @@ const nodeshift = require('nodeshift');
 
 const options = {
   strictSSL: false
-  
 };
 
 nodeshift.deploy(options).then(() => {
   console.log('Application deployed.');
 }).catch(error => {
   console.error('Error', error);
+  
 });
